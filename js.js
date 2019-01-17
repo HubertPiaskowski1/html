708dbe5ed0fb26ef32f0340efc7fef2ebf1ca63c
HubertPiaskowski <43027496+HubertPiaskowski@users.noreply.github.com>
2019-01-17 17:01:27 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,87 @@
+/**
+console.log("Hello - plik zewnetrzny")
+
+console.trace("sledzimy tylko");
+
+console.warn("Tutaj wystapil blad!");
+
+console.error("Tutaj wystapil blad!");
+
+let komputer = {
+						ram : 8,
+						monitor : ["Dell","Dell"] 
+						};
+						
+console.log(komputer);
+
+console.table(komputer);
+
+
+
+
+let imie = "Hubert";
+let nazwisko = "Piaskowski";
+let spacja = " ";
+
+let imie_i_nazwisko = imie + spacja + nazwisko;
+
+let imie_i_nazwisko2 = `<h1>${imie} ${nazwisko}</h1>`;
+
+document.write(imie_i_nazwisko2);
+
+**/
+
+
+let imie=window.prompt("Wpisz imie");
+console.log(imie)
+let nazwisko = window.prompt("Wpisz nazwisko");
+console.log(nazwisko)
+let telefon = window.prompt("Wpisz telefon");
+console.log(telefon)
+let email = window.prompt("Wpisz email");
+console.log(email)
+
+
+	
+
+
+
+let tresc = document.getElementById("tresc");
+console.log(tresc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
